c854360aee26ce27bc6798e6a2a86934cc4c2931
OlivierRaginel <OlivierRaginel@0b4bb1d4-4e5a-0410-9cc4-b2b747904278>
2012-05-01 14:07:26 +0000

    Item11808: perltidy them all, so people are not accidentally impacted by the new enforced rule
    git-svn-id: http://svn.foswiki.org/trunk/CalendarPlugin@14680 0b4bb1d4-4e5a-0410-9cc4-b2b747904278

--- lib/Foswiki/Plugins/CalendarPlugin/build.pl
+++ lib/Foswiki/Plugins/CalendarPlugin/build.pl
@@ -2,36 +2,38 @@
 
 # Standard preamble
 BEGIN {
-  foreach my $pc (split(/:/, $ENV{FOSWIKI_LIBS})) {
-    unshift @INC, $pc;
-  }
+    foreach my $pc ( split( /:/, $ENV{FOSWIKI_LIBS} ) ) {
+        unshift @INC, $pc;
+    }
 }
 
 use Foswiki::Contrib::Build;
 
 # Declare our build package
-{ package CalendarPluginBuild;
+{
 
-  @CalendarPluginBuild::ISA = ( "Foswiki::Contrib::Build" );
+    package CalendarPluginBuild;
 
-  sub new {
-    my $class = shift;
-    return bless( $class->SUPER::new( "CalendarPlugin" ), $class );
-  }
+    @CalendarPluginBuild::ISA = ("Foswiki::Contrib::Build");
 
-  # Example: Override the build target
-  sub target_build {
-    my $this = shift;
+    sub new {
+        my $class = shift;
+        return bless( $class->SUPER::new("CalendarPlugin"), $class );
+    }
 
-    $this->SUPER::target_build();
+    # Example: Override the build target
+    sub target_build {
+        my $this = shift;
 
-    # Do other build stuff here
-  }
+        $this->SUPER::target_build();
+
+        # Do other build stuff here
+    }
 }
 
 # Create the build object
 $build = new CalendarPluginBuild();
 
 # Build the target on the command line, or the default target
-$build->build($build->{target});
+$build->build( $build->{target} );
 

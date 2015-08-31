cb1645aa565c040a4586ec8d1f569cde056e8f65
George Clark <geonwiki@fenachrone.com>
2015-08-30 23:42:54 -0400

    Item11202: Prepare for release
     - Add the DEPENDENCIES file to the distribution so that Foswiki 2.0 can
       report Dependencies.
     - Update copyright,
     - Change to decimal VERSION and RELEASE
     - Add .gitignore

--- lib/Foswiki/Plugins/CalendarPlugin.pm
+++ lib/Foswiki/Plugins/CalendarPlugin.pm
@@ -6,8 +6,8 @@ use strict;
 use warnings;
 
 # See plugin topic for complete release history
-our $VERSION           = '$Rev$';
-our $RELEASE           = '2.001';
+our $VERSION           = '2.010';
+our $RELEASE           = '2.010';
 our $SHORTDESCRIPTION  = 'Show a monthly calendar with highlighted events';
 our $NO_PREFS_IN_TOPIC = 1;
 
@@ -30,4 +30,4 @@ Copyright (C) 2001 Andrea Sterbini, a.sterbini@flashnet.it
 Christian Schultze: debugging, relative month/year, highlight today
 Akim Demaille <akim@freefriends.org>: handle date intervals.
 Copyright (C) 2002-2006 Peter Thoeny, peter@thoeny.org
-Copyright (C) 2008-2011 Foswiki Contributors
+Copyright (C) 2008-2015 Foswiki Contributors
